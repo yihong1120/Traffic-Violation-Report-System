b5800d5e46c2b0f505a906c8666c203eccb28983
yihong1120 <qazsx748596@gmail.com>
2023-10-31 21:36:51 +0800

    Design initial structure of the server and web

---
+++ reports/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'reports/home.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'reports/dashboard.html')
