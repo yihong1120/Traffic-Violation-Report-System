93a30e91016f09820d06a978c68a8b027a1fd750
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2023-11-16 13:15:26 +0800

    Add type hints, docstring and comments to bigquery_utils.py

--- reports/bigquery_utils.py
+++ reports/bigquery_utils.py
@@ -1,8 +1,21 @@
-from django.http import JsonResponse
+from typing import List, Dict
+from django.http import JsonResponse, HttpRequest
 from google.cloud import bigquery
 
-def get_traffic_violations(request):
+def get_traffic_violations(request: HttpRequest) -> JsonResponse:
+    """
+    Query traffic violation data from BigQuery and return as JSON.
+
+    Args:
+        request (HttpRequest): The incoming request.
+
+    Returns:
+        JsonResponse: The JSON response containing the traffic violation data.
+    """
+    # Instantiate a BigQuery client.
     client = bigquery.Client()
+
+    # Define the SQL query.
     query = """
     SELECT 
         v.license_plate, 
@@ -19,10 +32,14 @@ def get_traffic_violations(request):
     LEFT JOIN 
         `traffic_violation_db.reports_mediafile` m ON v.traffic_violation_id = m.traffic_violation_id
     """
+    
+    # Execute the query.
     query_job = client.query(query)
 
+    # Get the query results.
     results = query_job.result()
 
+    # Prepare the data for the JSON response.
     data = []
     for row in results:
         lat, lng = map(float, row.location.split(','))
@@ -33,19 +50,27 @@ def get_traffic_violations(request):
             'media': row.file if row.file else 'path/to/default/image.jpg'
         })
 
-    # return data
+    # Return the data as a JSON response.
     return JsonResponse(data, safe=False) 
 
-def save_to_bigquery(traffic_violation, media_files):
+def save_to_bigquery(traffic_violation: 'TrafficViolation', media_files: List['MediaFile']) -> None:
+    """
+    Save traffic violation and media file data to BigQuery.
+
+    Args:
+        traffic_violation (TrafficViolation): The traffic violation data to save.
+        media_files (List[MediaFile]): The media file data to save.
+    """
+    # Instantiate a BigQuery client.
     client = bigquery.Client()
 
-    # 指定 dataset ID 和 table ID
+    # Specify the project ID, dataset ID, and table IDs.
     project_id = "pivotal-equinox-404812"
     dataset_id = "traffic_violation_db"
     table_id_violation = "reports_trafficviolation"
     table_id_media = "reports_mediafile"
 
-    # 准备要插入的违章数据
+    # Prepare the traffic violation data to insert.
     rows_to_insert_violation = [
         {
             "license_plate": traffic_violation.license_plate,
@@ -59,23 +84,23 @@ def save_to_bigquery(traffic_violation, media_files):
         },
     ]
 
-    # 插入违章数据
+    # Insert the traffic violation data.
     dataset_ref = client.dataset(dataset_id, project=project_id)
     table_ref_violation = dataset_ref.table(table_id_violation)
     table_violation = client.get_table(table_ref_violation)
     client.insert_rows_json(table_violation, rows_to_insert_violation)
 
-    # 准备要插入的媒体文件数据
+    # Prepare the media file data to insert.
     rows_to_insert_media = [
         {
             "traffic_violation_id": traffic_violation.id,
             "file": media_file.file.name,
-            # 其他需要的字段
+            # Additional fields as needed.
         }
         for media_file in media_files
     ]
 
-    # 插入媒体文件数据
+    # Insert the media file data.
     table_ref_media = dataset_ref.table(table_id_media)
     table_media = client.get_table(table_ref_media)
     client.insert_rows_json(table_media, rows_to_insert_media)
