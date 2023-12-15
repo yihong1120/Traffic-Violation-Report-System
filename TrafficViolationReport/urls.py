87e17aa9b290ff7f68d16a224e8579f4cf08db18
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2023-12-15 06:46:54 +0000

    Set up router of urls

--- TrafficViolationReport/urls.py
+++ TrafficViolationReport/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,8 +8,7 @@ urlpatterns = [
     path('reports/', include('reports.urls', namespace='reports')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('traffic_data/', include('traffic_data.urls', namespace='traffic_data')),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('', include('traffic_data.urls', namespace='traffic_data')),
+    ## AI model application
 ]
 
 # 在开发环境中服务媒体文件
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = 'accounts'
@@ -9,6 +10,7 @@ urlpatterns = [
     path('verify/', views.verify, name='verify'),
     path('account/', views.account_view, name='account'),
     path('validate-username-email/', views.validate_username_email, name='validate-username-email'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     # 包含 allauth 的 URL
     path('', include('allauth.urls')),
 ]
