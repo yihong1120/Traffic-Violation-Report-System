20c65fa5438168ff06c8a3c04eb28f215ee41adf
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2023-12-16 17:53:20 +0000

    Refractor the code

--- traffic_data/urls.py
+++ traffic_data/urls.py
@@ -5,9 +5,9 @@ app_name = 'traffic_data'
 
 urlpatterns = [
     # path('', views.home, name='home'),
-    path('traffic-violation-details/<str:traffic_violation_id>/', views.get_traffic_violation_details, name='traffic-violation-details'),
+    path('traffic-violation-details/<str:traffic_violation_id>/', views.traffic_violation_details_view, name='traffic-violation-details'),
     path('search-traffic-violations/', views.search_traffic_violations_view, name='search-traffic-violations'),
-    path('traffic-violation-markers/', views.get_traffic_violation_markers, name='traffic-violation-markers'),
+    path('traffic-violation-markers/', views.traffic_violation_markers_view, name='traffic-violation-markers'),
     
     # path('', some_app_views.home_view, name='home'),  # Add this line for the root URL
 ]
