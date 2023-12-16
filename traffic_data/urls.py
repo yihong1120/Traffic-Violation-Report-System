f2020a5f2666fe44b8db9bc7a523cdaf32aaf1c1
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2023-12-16 17:57:41 +0000

    Refractor the code

--- traffic_data/urls.py
+++ traffic_data/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'traffic_data'
 
 urlpatterns = [
-    # path('', views.home, name='home'),
     path('traffic-violation-details/<str:traffic_violation_id>/', views.traffic_violation_details_view, name='traffic-violation-details'),
     path('search-traffic-violations/', views.search_traffic_violations_view, name='search-traffic-violations'),
     path('traffic-violation-markers/', views.traffic_violation_markers_view, name='traffic-violation-markers'),
