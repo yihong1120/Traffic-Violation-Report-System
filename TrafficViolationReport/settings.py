a87f229bb999848e1df5b8b71f26ca4e894654ae
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2023-12-26 22:05:14 +0800

    Update settings.py

--- TrafficViolationReport/settings.py
+++ TrafficViolationReport/settings.py
@@ -159,11 +159,11 @@ LOGOUT_REDIRECT_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = config.get('EMAIL_HOST_USER')
 if not EMAIL_HOST_USER:
     raise ValueError("No 'EMAIL_HOST_USER' has been set in the configuration.")
 
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = config.get('EMAIL_HOST_PASSWORD')
 if not EMAIL_HOST_PASSWORD:
     raise ValueError("No 'EMAIL_HOST_PASSWORD' has been set in the configuration.")
 
