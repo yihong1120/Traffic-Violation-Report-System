498e99c9bf0c7b5b3be9f2468a9c2c7ac0359fa4
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2024-01-02 09:20:07 +0000

    Utilise PATH instead of os to create TEMPLATES route

--- TrafficViolationReport/settings.py
+++ TrafficViolationReport/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'TrafficViolationReport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
