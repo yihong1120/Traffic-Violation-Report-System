54d480232d084ac26e6fca33f9082c1c276bdfa4
yihong1120 <qazsx748596@gmail.com>
2024-01-11 01:44:55 +0800

    Add TrafficViolationMarkerSerializer

--- traffic_data/serializers.py
+++ traffic_data/serializers.py
@@ -5,3 +5,8 @@ class TrafficViolationSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrafficViolation
         fields = '__all__'
+
+class TrafficViolationMarkerSerializer(serializers.Serializer):
+    traffic_violation_id = serializers.UUIDField()
+    lat = serializers.FloatField()
+    lng = serializers.FloatField()
