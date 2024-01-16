78cf68a4e9bc5964c3c549abeb2c06bb6af60bd8
yihong1120 <qazsx748596@gmail.com>
2024-01-17 00:35:16 +0800

    Update serializers:combine MediaFile and TrafficViolation

--- reports/serializers.py
+++ reports/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import TrafficViolation, MediaFile
 
-class TrafficViolationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TrafficViolation
-        fields = '__all__'
-
 class MediaFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = MediaFile
+        fields = '__all__'
+
+class TrafficViolationSerializer(serializers.ModelSerializer):
+    media_files = MediaFileSerializer(many=True)  # 嵌套序列化媒體資訊
+
+    class Meta:
+        model = TrafficViolation
         fields = '__all__'
