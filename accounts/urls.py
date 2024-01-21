a3abe2edb89a0d2359d18cf376f08b0ad992af3a
yihong1120 <qazsx748596@gmail.com>
2024-01-21 16:14:24 +0800

    Seperate usage of general views and api views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
-from . import views
+from . import views, api_views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    # WEB url
+    # WEB path
     path('login/', views.login, name='login'),
     # path('register/', views.register, name='register'),
     path('verify/', views.verify, name='verify'),
@@ -21,19 +21,20 @@ urlpatterns = [
     # path('', include('allauth.urls')),
     # path('accounts/', include('allauth.urls')),
 
-    # API url
-    path('api/login/', views.login_api, name='api_login'),
-    path('api/logout/', views.logout_api, name='api_logout'),
-    path('register/', views.register_api, name='api_register'),
-    path('api/verify/', views.verify_api, name='api_verify'),
-    path('api/account/', views.account_api, name='api_account'),
-    path('api/validate-username-email/', views.validate_username_email, name='api_validate-username-email'),
+    # API path
+    path('api/login/', api_views.login_api, name='api_login'),
+    path('api/logout/', api_views.logout_api, name='api_logout'),
+    path('api/get-user-info/', api_views.get_user_info_api, name='api_get_user_info'),
+    path('api/register/', api_views.register_api, name='api_register'),
+    path('api/verify/', api_views.verify_api, name='api_verify'),
+    path('api/account/', api_views.account_api, name='api_account'),
+    path('api/validate-username-email/', views.validate_username_email, name='api_validate_username_email'),
     path('api/logout/', LogoutView.as_view(), name='api_logout'),
-    path('api/password/change/', views.custom_password_change_api, name='api_password_change'),
-    path('api/password/change/done/', views.password_change_done_api, name='api_password_change_done'),
-    path('api/email/change/', views.email_change_api, name='api_email_change'),
-    path('api/social-connections/', views.social_account_connections_api, name='api_socialaccount_connections'),
-    path('api/delete-account/', views.account_delete_api, name='api_account_delete'),
+    path('api/password-change/', api_views.custom_password_change_api, name='api_password_change'),
+    path('api/password-change-done/', api_views.password_change_done_api, name='api_password_change_done'),
+    path('api/email-change/', api_views.email_change_api, name='api_email_change'),
+    path('api/social-connections/', api_views.social_account_connections_api, name='api_socialaccount_connections'),
+    path('api/delete-account/', api_views.account_delete_api, name='api_account_delete'),
     # path('', include('allauth.urls')),
     # path('accounts/', include('allauth.urls')),
 ]
