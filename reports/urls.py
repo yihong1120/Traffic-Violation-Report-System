25e534b15a58486f0dab9e6207c861d2241dfaca
yihong1120 <qazsx748596@gmail.com>
2024-01-22 00:20:27 +0800

    Seperate usage of general views and api views

--- reports/urls.py
+++ reports/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from . import views
+from . import views, api_views
 
 app_name = 'reports'
 
 urlpatterns = [
+    # WEB path
     # path('edit-report/', views.edit_report, name='edit_report'),
     path('upload/', views.dashboard, name='file_upload'),
     # path('dashboard/', views.dashboard, name='dashboard'),
-    path('api/submit-traffic-violation/', views.submit_traffic_violation_api, name='submit_traffic_violation'),
-    path('api/traffic-violations-list/', views.traffic_violation_list_api, name='traffic_violation_list'),
-    path('api/traffic-violations-detail/', views.traffic_violation_detail_api, name='traffic_violation_detail'),
+
+    # API path
+    path('api/create-report/', api_views.create_report_api, name='api_create_report'),
+    path('api/traffic-violations-list/', api_views.traffic_violation_list_api, name='api_traffic_violations_list'),
+    path('api/traffic-violations-detail/', api_views.traffic_violation_detail_api, name='api_traffic_violation_detail'),
+    path('api/update-report/<uuid:violation_id>/', api_views.update_report_api, name='api_update_report'),
 ]
