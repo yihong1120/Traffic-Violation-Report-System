000ed135bd37f22a6867dfe803a1b94df71fe8c5
yihong1120 <qazsx748596@gmail.com>
2024-01-24 00:08:27 +0800

    Refactor the code: usage of User model

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import UserProfile
+from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,6 +8,8 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['username', 'email']
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)  # 在这里添加
+
     class Meta:
         model = UserProfile
         fields = ['user', 'email_verified', 'email_verified_code', 'verification_code_expiry']
