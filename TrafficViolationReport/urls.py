fa3c3610262ec6e967313358d537bb412cec361d
yihong1120 <qazsx748596@gmail.com>
2024-01-28 01:43:29 +0800

    Refactor the code

--- TrafficViolationReport/urls.py
+++ TrafficViolationReport/urls.py
@@ -3,32 +3,43 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
+
+#Import JWT view
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# Import your view
 from traffic_data.views import home
 from reports.views import dashboard, edit_report
 from accounts.views import login, register, account_view
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # 包含您的應用的 urls
-    path('', home, name='home'),
-    path('dashboard/', dashboard, name='dashboard'),
-    path('edit-report/', edit_report, name='edit_report'),
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('account/', account_view, name='account_view'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-
-    path('reports/', include('reports.urls', namespace='reports')),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('', include('traffic_data.urls', namespace='traffic_data')),
-    path('license_plate_insights/', include('license_plate_insights.urls', namespace='license_plate_insights')),
-    path('llm_customer_service/', include('llm_customer_service.urls', namespace='llm_customer_service')),
+     # Management background
+     path('admin/', admin.site.urls),
+    
+     # JWT Authentication URL
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+     # URL of the core application
+     path('', home, name='home'),
+     path('dashboard/', dashboard, name='dashboard'),
+     path('edit-report/', edit_report, name='edit_report'),
+    
+     #User authentication related URL
+     path('login/', login, name='login'),
+     path('register/', register, name='register'),
+     path('account/', account_view, name='account_view'),
+     path('logout/', LogoutView.as_view(), name='logout'),
+    
+     # Contains URLs for other applications
+     path('reports/', include('reports.urls', namespace='reports')),
+     path('accounts/', include('accounts.urls', namespace='accounts')),
+     path('accounts/auth/', include('allauth.urls')),
+     path('', include('traffic_data.urls', namespace='traffic_data')),
+     path('license_plate_insights/', include('license_plate_insights.urls', namespace='license_plate_insights')),
+     path('llm_customer_service/', include('llm_customer_service.urls', namespace='llm_customer_service')),
 ]
 
-# 在開發環境中服務媒體文件
+# Provide media files in the development environment
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
